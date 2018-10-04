815eab7a128aa4cbd7bef45d73b7d2dafa350d82
Borderlander103 <mailbox@brianrichardsononline.com>
2018-10-04 09:13:25 -0700

    Finish version 1.0

--- app/src/main/java/com/github/borderlander103/higherorlower/MainActivity.java
+++ app/src/main/java/com/github/borderlander103/higherorlower/MainActivity.java
@@ -2,12 +2,54 @@ package com.github.borderlander103.higherorlower;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    int mRandom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        randomNumber();
     }
+
+    public void randomNumber() {
+        Random r = new Random();
+        mRandom = r.nextInt(20) + 1;
+    }
+
+    public void guessButton(View view) {
+
+        Log.i("Value", Integer.toString(mRandom));
+
+        EditText mGuessView = findViewById(R.id.numberEditText);
+
+        String mGuessString = mGuessView.getText().toString();
+
+        int mGuessInt = Integer.parseInt(mGuessString);
+
+        Log.i("Value", Integer.toString(mGuessInt));
+
+        String message;
+
+        if (mGuessInt > mRandom) {
+            message = "Lower";
+        } else if (mGuessInt < mRandom){
+            message = "Higher";
+        } else {
+            message = "Just Right";
+        }
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+
+    }
+
 }
